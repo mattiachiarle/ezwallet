29f78ebc574e615808c80b346e60e0ba56e02ac6
MATTIA CHIARLE s308472 <s308472@studenti.polito.it>
2023-06-08 15:33:32 +0200

    Fix on createGroup

--- code/controllers/users.js
+++ code/controllers/users.js
@@ -131,8 +131,10 @@ export const createGroup = async (req, res) => {
     }
     
     const newGroup = await Group.create({ name: name, members: membersAdded });
+
+    const addedEmails = newGroup.members.map((m) => {return {email: m.email}});
     
-    res.status(200).json({ data: { group: newGroup, alreadyInGroup: alreadyInGroup, membersNotFound: membersNotFound }, refreshedTokenMessage: res.locals.refreshedTokenMessage });
+    res.status(200).json({ data: { group: {name: newGroup.name, members: addedEmails}, alreadyInGroup: alreadyInGroup, membersNotFound: membersNotFound }, refreshedTokenMessage: res.locals.refreshedTokenMessage });
 
   } catch (err) {
     res.status(500).json({error: err.message})
