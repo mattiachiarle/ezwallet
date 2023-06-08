e2ffe9b9f02088a88a50e604f77eff816b619e80
NUNZIO MESSINEO s315067 <s315067@studenti.polito.it>
2023-06-08 19:23:02 +0200

    starting utils integration tests

--- code/test/utils.integration.test.js
+++ code/test/utils.integration.test.js
@@ -1,19 +1,22 @@
 import { handleDateFilterParams, verifyAuth, handleAmountFilterParams } from '../controllers/utils';
 
 describe("handleDateFilterParams", () => { 
-    test('Dummy test, change it', () => {
+    test('Dummy test, change it', (done) => {
         expect(true).toBe(true);
+        done()
     });
 })
 
 describe("verifyAuth", () => { 
-    test('Dummy test, change it', () => {  
+    test('Dummy test, change it', (done) => {  
         expect(true).toBe(true);  
+        done()
     });
 })
 
 describe("handleAmountFilterParams", () => { 
-    test('Dummy test, change it', () => {  
-        expect(true).toBe(true);  
+    test('Dummy test, change it', (done) => {  
+        expect(true).toBe(true); 
+        done() 
     });
 })
